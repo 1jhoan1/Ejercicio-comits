6db570fbdd36af48738342279d0ccb5bad620229
1jhoan1 <jhoan.pinton@upb.edu.co>
2025-10-06 22:12:39 -0500

    Escribe un algoritmo que lea tres números y determine el mayor de ellos.
    encontrar el mayor de tres numeros

---
+++ 2dorejercicio.cpp
@@ -0,0 +1,18 @@
+// Ejercicio 34
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Digite tres numeros: ";
+    cin >> a >> b >> c;
+
+    if (a > b && a > c)
+        cout << "El mayor es: " << a;
+    else if (b > c)
+        cout << "El mayor es: " << b;
+    else
+        cout << "El mayor es: " << c;
+
+    return 0;
+}
