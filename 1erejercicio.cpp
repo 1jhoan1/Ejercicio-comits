541022deb3c222374be35e5740c88c131cee7f9e
1jhoan1 <jhoan.pinton@upb.edu.co>
2025-10-06 22:09:49 -0500

    Escribe un algoritmo que lea dos números y determine el mayor de ellos.
    determinar el mayor entre dos numeros

---
+++ 1erejercicio.cpp
@@ -0,0 +1,16 @@
+// Ejercicio 33
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Digite dos numeros: ";
+    cin >> a >> b;
+
+    if (a > b)
+        cout << "El mayor es: " << a;
+    else
+        cout << "El mayor es: " << b;
+
+    return 0;
+}
